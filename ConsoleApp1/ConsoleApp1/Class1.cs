8a0bf7363beacf3bd349e971a6681b1d2c7bf08f
chrismaldona2 <chris.ariel.maldonado@gmail.com>
2023-04-18 19:08:24 -0300

    Primer Commit

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            double num;
+            List<double> numeros = new List<double> { 1, 2, 3, 4};
+            num = Convert.ToDouble(Console.ReadLine());
+            numeros.Add(num);
+            Console.WriteLine("Resultado: \n" + "Los elementos de la lista son: " + string.Join(", ", numeros) + ".");
+
+        }
+    }
+}
